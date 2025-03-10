a7d3935646ba92ec6097b05740fa6e7e7efd639b
unknown <YY215@ccs.local>
2025-03-10 05:57:16 -0700

    Add question1.js

---
+++ question1.js
@@ -0,0 +1,11 @@
+// Initialize array of numbers
+const arrayOfNum = [1, 2, 3, 4, 5, 6, 7, 8, ,9, 10];
+
+
+// Funtion that will filter out even numbers 
+function filterEvenNumbers() {
+    return filterEvenNumbers = arrayOfNum.filter(num => num % 2 === 0);
+}
+
+// Printing output
+console.log(filterEvenNumbers(filterEvenNumbers));
